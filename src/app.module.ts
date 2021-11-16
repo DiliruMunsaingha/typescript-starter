c01228f5327e1eae0c55681de9b7072d80032a6c
DiliruMunasingha <diliru@cabbageapps.com>
2021-11-16 12:10:36 +0530

    Initial Commit has error in authentication service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PostsModule } from './posts/posts.module';
+import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { ExceptionLoggerFilter } from './utils/exceptionsLogger.filter';
+import { APP_FILTER } from '@nestjs/core';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthenticationModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionLoggerFilter,
+    },
+  ],
 })
 export class AppModule {}
